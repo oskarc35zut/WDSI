f414aedf60a70f5ab75fd6e2c453455d86256102
Oskar | PC - Windows GitHub for Desktop <oskarc35@gmail.com>
2017-04-04 23:13:55 +0200

    connect 4 v0.001

---
+++ WdSI/Laboratory2/Laboratory2/Connect4State.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laboratory2
+{
+    class Connect4State : State
+    {
+        private static int size;
+
+        public static int Size
+        {
+            get { return Connect4State.size; }
+        }
+
+        int[,] table;
+
+        int[,] Table
+        {
+            get { return this.table;  }
+            set { this.table = value; }
+        }
+
+        private string id;
+
+        public override string ID
+        {
+            get { return this.id; }
+        }
+
+        public override double ComputeHeuristicGrade()
+        {
+            throw new NotImplementedException();
+        }
+        public Connect4State(int size) : base() //konstruktor inicjujący
+        {
+            Connect4State.size = size;
+
+            table = new int[Size, Size];
+
+            //table 0 builder
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Table[i,j] = 0;
+                }
+            }
+
+            //id builder
+            id = "";
+            for (int i = 0; i < Size*Size; i++)
+            {
+                this.id += 0;
+            }
+
+
+        }
+
+        public Connect4State(Connect4State parent, int[,] tab) : base(parent)
+        {
+            // reszta implementacji
+
+            // ustawienie stringa identyfikujacego stan .
+            //id builder
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    id += Table[i, j];
+                }
+            }
+            // ustawienie na ktorym poziomie w drzwie znajduje sie stan .
+            this.depth = parent.depth + 0.5;
+
+            // Bardzo wazne nie ustawiany na czubek drzewa z ktorego budujemy stany.Tylko na pierwsze pokolenie stanow potomnych
+            if (parent.rootMove == null)
+            {
+                this.rootMove = this.id;
+            }
+            else {
+                this.rootMove = parent.rootMove;
+            }
+        }
+    }
+}
--- WdSI/Laboratory2/Laboratory2/Program.cs
+++ WdSI/Laboratory2/Laboratory2/Program.cs
@@ -6,7 +6,10 @@ using System.Text;
 namespace Laboratory2 {
     class Program {
         static void Main(string[] args) {
+
+            int size = 5;
             Console.WriteLine("Hello World");
+            Console.Read();
         }
     }
 }
