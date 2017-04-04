10f4d01e44362fc52c61e2b8d9c9e979e0b74aad
Oskar | PC - Windows GitHub for Desktop <oskarc35@gmail.com>
2017-04-04 13:08:47 +0200

    connect4 init

---
+++ WdSI/Laboratory2/Laboratory2/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laboratory2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
