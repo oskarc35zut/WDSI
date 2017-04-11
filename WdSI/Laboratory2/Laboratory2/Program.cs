2c35a516e8fa24df71867c17f6de083124341442
PC - Windows <gthemaniacpl@gmail.com>
2017-04-12 00:46:38 +0200

    connect4 niby dziala vol2

--- WdSI/Laboratory2/Laboratory2/Program.cs
+++ WdSI/Laboratory2/Laboratory2/Program.cs
@@ -1,8 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Laboratory2 {
     class Program {
@@ -11,9 +13,27 @@ namespace Laboratory2 {
             Console.CursorVisible = false;
             Console.Write(" Connect4");
 
+            int who = 2;
             int width = 7;
             int heigth = 4;
-            int deep = 1;
+            int deep = 3;
+
+            DialogResult dialogResult_sudoku = MessageBox.Show("Komputer zaczyna? ( ͡° ͜ʖ ͡°)", "", MessageBoxButtons.YesNo);
+            if (dialogResult_sudoku == DialogResult.Yes)
+            {
+                who++;
+            }
+
+            Console.Write("\nJak gleboko szukac: ");
+            deep = Console.Read()-48;
+
+            Console.Clear();
+            Console.SetWindowPosition(0, 0);
+            Console.Write(" Connect4");
+
+
+
+
             Connect4State.Init(width, heigth, deep);
 
             //Connect4State parrent = new Connect4State();
@@ -34,21 +54,28 @@ namespace Laboratory2 {
                 }
             }
             
-            int who = 2;
+            
             while (true)
             {
-                //table = Connect4State.ComputerChoiceTable(table, who % 2 == 0 ? 1 : 2);
-                table = Connect4State.Move(table, Connect4State.GetChoise(table, who % 2 == 0 ? 1 : 2), who % 2 == 0 ? 1 : 2);
-                if (Connect4State.isWin(who % 2 == 0 ? 1 : 2, table)) break;
-                who++;
+                switch (who%2)
+                {
+                    case 0:
+                        table = Connect4State.Move(table, Connect4State.GetChoise(table, who % 2 == 0 ? 1 : 2), who % 2 == 0 ? 1 : 2);
+                        break;
 
-                Thread.Sleep(200);
-                //table = Connect4State.Move(table, Connect4State.GetChoise(table, who % 2 == 0 ? 1 : 2), who % 2 == 0 ? 1 : 2);
-                
-                table = Connect4State.ComputerChoiceTable(table, who % 2 == 0 ? 1 : 2);
+                    case 1:
+                        Thread.Sleep(200);
+                        table = Connect4State.ComputerChoiceTable(table, who % 2 == 0 ? 1 : 2);
+                        //Thread.Sleep(300);
+                        break;
+
+
+                    default:
+                        break;
+                }
+
                 if (Connect4State.isWin(who % 2 == 0 ? 1 : 2, table)) break;
                 who++;
-                Thread.Sleep(300);
             }
 
             Connect4State.Print(table);
